950673dcf886fb0736dba34b41358e5ba542d7d7
Louetta Daly <500000504@my.fiveislands.uwi.edu>
2023-11-02 19:41:49 -0400

    Finetuning, added js comments and improving respnsiveness

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 /* Add your JavaScript to this file */
 document.addEventListener("DOMContentLoaded", function(){
     "use strict";
+    //set variables for email input, form with post action and message class
     const emailIn = document.querySelector('#email');
     const form = document.querySelector('form');
     let message = document.querySelector('.message');
 
+    //'Listen' user to submit form and provide output 
     form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        const email = emailIn.value;
+        e.preventDefault();     //prevent the browsers' default handling of the submission
+        const email = emailIn.value;    //set variable for the email input value for printing in the confirmation message
+        //check if email is valid and prints confirmation, if not print error message
         if (isValid(email)) {
         message.textContent = "Thank you! Your email address " + emailIn.value + " has been added to our mailing list!";
         } else {
@@ -15,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     });
 
+    //Function to validate email address using email Regex
     function isValid(email) {
         const emailReg = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
         return emailReg.test(email);
